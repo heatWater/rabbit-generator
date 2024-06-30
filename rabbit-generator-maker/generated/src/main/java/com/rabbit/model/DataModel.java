b66507fa811af470ce0fb4e201935a1a58e90d63
heatWater <1919406470@qq.com>
2024-06-30 22:19:53 +0800

    制作工具开发 v11

---
+++ rabbit-generator-maker/generated/src/main/java/com/rabbit/model/DataModel.java
@@ -0,0 +1,29 @@
+package com.rabbit.maker.model;
+
+import lombok.Data;
+
+/**
+ * 动态模板配置
+ */
+@Data
+public class DataModel {
+
+
+    /**
+     * 是否生成循环
+     */
+    private boolean loop  = false;
+
+
+    /**
+     * 作者注释
+     */
+    private String author  = "sean";
+
+
+    /**
+     * 输出信息
+     */
+    private String outputText  = "sum = ";
+
+}
---
+++ rabbit-generator-maker/src/main/resources/templates/README.md.ftl
@@ -0,0 +1,13 @@
+# ${name}
+
+> ${description}
+>
+> 作者：${author}
+>
+> 基于 [程序员鱼皮](https://yuyuanweb.feishu.cn/wiki/Abldw5WkjidySxkKxU2cQdAtnah) 的 [鱼籽代码生成器项目](https://github.com/liyupi/yuzi-generator) 制作，感谢您的使用！
+
+可以通过命令行交互式输入的方式动态生成想要的项目代码
+
+## 使用说明
+
+执行项目根目录下的脚本文件：
